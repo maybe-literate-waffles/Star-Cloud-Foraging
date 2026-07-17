5393219c9a6a3fb27d1832f031c4e56c9a6bfff1
literate_waffle <learny023@gmail.com>
2026-07-17 20:02:41 +0530

    initial commit: First commit for the project :) feat: Built the scraping/get function

---
+++ .ipynb_checkpoints/clean_up-checkpoint.py
@@ -0,0 +1,3 @@
+from scraping import get_data
+
+print(get_data())
---
+++ scraping.py
@@ -0,0 +1,19 @@
+import requests as rq
+
+BASE_URL = "https://api.open-meteo.com/v1"
+LATITUDE = "latitude=35.6762,51.5074,1.3521,40.7128,-33.8688"
+LONGITUDE = "longitude=139.6503,-0.1278,103.8198,-74.0060,151.2093"
+TIMEZONE = "timezone=Asia%2FTokyo,Europe%2FLondon,Asia%2FSingapore,America%2FNew_York,Australia%2FSydney"
+
+
+def get_data():
+    full_url = (
+        f"{BASE_URL}/forecast?{LATITUDE}&{LONGITUDE}&current=temperature_2m&{TIMEZONE}"
+    )
+    response = rq.get(full_url)
+    return response.json()
+
+
+response = get_data()
+
+print(response)
